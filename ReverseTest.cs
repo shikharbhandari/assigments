593d2b0f5283faca91957c62e2aae5ebc17fab46
shikharbhandari <51962676+shikharbhandari@users.noreply.github.com>
2019-06-20 23:10:12 +0530

    Add files via upload

---
+++ ReverseTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using reversestring;
+using System.Collections.Generic;
+
+namespace ReverseTringTest
+{
+    [TestClass]
+    public class ReverseTest
+    {
+        [TestMethod]
+        public void TestFormetho1()
+        {
+            //Arrange
+            string input = "qqwerty";
+            string output = "ytrewqq";
+
+            //Act
+            var result = Program.Revwithstack(input);
+            //Assert
+            Assert.AreEqual(output,result);
+        }
+        [TestMethod]
+        public void TestFormethod2()
+        {
+            //Arrange
+            string input = "qqwerty";
+            string output = "ytrewqq";
+
+            //Act
+            var result = Program.ReverseString(input);
+            //Assert
+            Assert.AreEqual(output, result);
+        }
+
+        [TestMethod]
+        public void TestFormethod3()
+        {
+            //Arrange
+            string input = "qqwerty";
+            string output = "ytrewqq";
+
+            //Act
+            var result = Program.ReverseNostack(input);
+            //Assert
+            Assert.AreEqual(output, result);
+        }
+    }
+}
