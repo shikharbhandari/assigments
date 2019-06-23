fe4f3a8ccc11d0429294e041fd8b3b470cc2c94d
shikharbhandari <51962676+shikharbhandari@users.noreply.github.com>
2019-06-24 00:19:49 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,96 +6,119 @@ using System.Threading.Tasks;
 
 namespace StackOperations
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
             Stack<string> MyStack = new Stack<string>();
-            GetInputString(MyStack);
-            Console.ReadKey();
-
-        }
-        public static void GetInputString(Stack<string> MyStack)
-        {
-            string input;
-            Console.WriteLine("Stack operations" + "1.push" +" "+ "2.pop" +" "+ "3.greatest number in stack"+" "+"4.exit");
-
-         
-                input = Console.ReadLine();
-            bool validIP = CheckInput(input);
-            if (validIP == false)
+            string input;           
+            while (true)
             {
-                Console.WriteLine("enter a valid option");
+                Getinput obj = new Getinput();
+                input =obj.Getuserinput();
+                MyStackclass mystack = new MyStackclass();
+                
+                //Program obj = new Program();
+                switch(input)
+                {
+                    case "1" :
+                        {
+                            Console.WriteLine("enter the number to push");
+                            string pushnumber = Console.ReadLine();
+
+                            MyStack = MyStackclass.Push(MyStack,pushnumber);
+                        }
+                        break;
+                    case "2" :
+                        MyStack = MyStackclass.Pop(MyStack);
+                        break;
+                    case "3" :
+                        var newstack = MyStackclass.MaxNumber(MyStack);
+                        break;
+                    case "4":
+                        return;                        
+                    default :
+                        Console.WriteLine("enter the valid input");
+                        break;
+                }
+            }
+        }            
+    }      
+    
+    public class MyStackclass
+    {
+        public static Stack<string> Push(Stack<string> Newstack,string pushnumber)
+        {
+            bool result1 = int.TryParse(pushnumber, out int result);
+            if (result1 == true)
+            {
+                Newstack.Push(pushnumber);
+                return Newstack;
             }
             else
             {
-
-                if (input == "1")
-                {
-                    Console.WriteLine("enter the number to push");
-                    string pushnumber = Console.ReadLine();
-                    MyStack = PushStack(pushnumber, MyStack);
-
-                    GetInputString(MyStack);
-
-                }
-                if (input == "2")
-                {
-                    MyStack = PopStack(MyStack);
-                    GetInputString(MyStack);
-                }
-                if (input == "3")
-                {
-                    MyStack = MaxNumber(MyStack);
-                    GetInputString(MyStack);
-                }
-                if (input == "4")
-                    return;
+                Console.WriteLine("enter a number");
+                return Newstack;
             }
+
         }
 
-        public static Stack<string> PushStack(string number,Stack<string> newstack)
-        {
-            
-            newstack.Push(number);
-            return newstack;
-        }
-        public static bool CheckInput(string input)
-        {
-            if (input == "1" || input == "2" || input == "3" || input == "4")
-                return true;
-            else
-                return false;
-        }
-
-        public static Stack<string> PopStack(Stack<string> Mystack)
-        {
-            Console.WriteLine("the poped element was" + Mystack.Pop());
-            return Mystack;
-        }
         public static Stack<string> MaxNumber(Stack<string> NewStack)
         {
             int? max = null;
             int? value = null;
-            foreach(var imt in NewStack)
+            foreach (var imt in NewStack)
             {
                 value = Convert.ToInt32(imt);
-                if(max==null)
+                if (max == null)
                 {
                     max = value;
                 }
-                else
+                else if (value > max)
                 {
-                    if(value>max)
-                    {
-                        max = value;
-                    }
+                    max = value;
+
                 }
 
             }
-            Console.WriteLine("largest number in stack is:" + max);
-            return NewStack;
+            //max = Convert.ToInt32(NewStack.Max());
+            if (NewStack.Count < 1)
+            {
+                Console.WriteLine("there is no element in the stack");
+                return NewStack;
+            }
+            else
+            {
+                Console.WriteLine("largest number in stack is:" + max);
+                return NewStack;
+            }
         }
-       
+
+        public static Stack<string> Pop(Stack<string> Newstack)
+        {
+            if (Newstack.Count < 1)
+            {
+                Console.WriteLine("there is no element to pop");
+                return Newstack;
+            }
+            else
+            {
+                Console.WriteLine("the poped element was" + Newstack.Pop());
+                return Newstack;
+            }
+        }
+     
     }
+    public class Getinput
+    {
+        public string Getuserinput()
+        {
+            string input=string.Empty;
+            Console.WriteLine("Stack operations" + "1.push" + " " + "2.pop" + " " + "3.greatest number in stack" + " " + "4.exit");
+            input=Console.ReadLine();
+            return input;
+
+        }
+    }
+
 }
---
+++ Stacktest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using StackOperations;
+using System.Linq;
+
+
+namespace Stackoperation_Test
+{
+    [TestClass]
+    public class Stacktest
+    {
+        [TestMethod]
+        public void Stack_push_test()
+        {
+            //arrange
+            Stack<string> mystack = new Stack<string>();
+            string pushnumber = "23";
+            //act
+            var result = MyStackclass.Push(mystack,pushnumber);
+            //assert
+            Assert.AreEqual(result.Pop(), pushnumber);
+        }
+
+        [TestMethod]
+        public void Stack_pop_test()
+        {
+            //arrange
+            Stack<string> mystack = new Stack<string>();
+            string pushnumber = "23";
+            string pushnumber1 = "55";
+            //act
+            var result = MyStackclass.Push(mystack, pushnumber);
+            var result1 = MyStackclass.Push(result, pushnumber1);
+            var result2 = MyStackclass.Pop(result1);
+            //assert
+            Assert.AreEqual(result2.Peek(), pushnumber);
+        }
+
+        [TestMethod]
+        public void Stack_pop_test1()
+        {
+            //arrange
+            Stack<string> mystack = new Stack<string>();            
+            //act
+            var result2 = MyStackclass.Pop(mystack);
+            //assert
+            Assert.AreEqual(result2,mystack);
+        }
+
+        [TestMethod]
+        public void Stack_push_test1()
+        {
+            //arrange
+            Stack<string> mystack = new Stack<string>();
+            string pushnumber = "ab";
+            //act
+            var result = MyStackclass.Push(mystack, pushnumber);
+            //assert
+            Assert.AreEqual(result, mystack);
+        }
+
+        [TestMethod]
+        public void Stack_max_test()
+        {
+            //arrange
+            Stack<string> mystack = new Stack<string>();
+            string pushnumber = "25";
+            string pushnumber1 = "55";
+
+            //act
+            var result = MyStackclass.Push(mystack, pushnumber);
+            var result1 = MyStackclass.Push(result, pushnumber1);
+
+            var result2 = MyStackclass.MaxNumber(result1);
+            //assert
+            Assert.AreEqual(pushnumber1, result2.Max());
+        }
+
+        [TestMethod]
+        public void Stack_max_test1()
+        {
+            //arrange
+            Stack<string> mystack = new Stack<string>();
+            //act           
+            var result2 = MyStackclass.MaxNumber(mystack);
+            //assert
+            Assert.AreEqual(mystack, result2);
+        }
+    }
+}
